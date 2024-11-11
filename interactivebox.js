0dd06369cd7c796356c4d65ab6747285ca361086
GokilaSundar <gokithilaga267@gmail.com>
2024-11-11 16:10:55 +0530

    Added Background color through javascript for look interactive

---
+++ interactivebox.js
@@ -0,0 +1,16 @@
+const radiobutton = document.querySelectorAll('input[type = "radio"]');
+radiobutton.forEach((radio) => {
+  const radioboxes = document.querySelectorAll(".radioboxContainer");
+
+  radio.addEventListener("change", () => {
+    radioboxes.forEach((container) => {
+      container.style.border = "2px solid black";
+      container.style.backgroundColor = "";
+    });
+    if (radio.checked) {
+      const selectedbox = radio.closest(".radioboxContainer");
+      selectedbox.style.border = "3px solid #ff6b82";
+      selectedbox.style.backgroundColor = "#f5f0f1";
+    }
+  });
+});
