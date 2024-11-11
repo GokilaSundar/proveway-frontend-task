0146cdd58143c5d501055c4d5e986d40664318a5
GokilaSundar <gokithilaga267@gmail.com>
2024-11-12 01:09:29 +0530

    Changed legend ,Media query and javascript file

--- interactivebox.js
+++ interactivebox.js
@@ -1,16 +1,30 @@
 const radiobutton = document.querySelectorAll('input[type = "radio"]');
-radiobutton.forEach((radio) => {
+radiobutton.forEach((radio, index) => {
   const radioboxes = document.querySelectorAll(".radioboxContainer");
+  const discountPrices = document.querySelectorAll(".discountPrice");
+  const totals = document.querySelector(".total");
 
   radio.addEventListener("change", () => {
-    radioboxes.forEach((container) => {
-      container.style.border = "2px solid rgb(213, 209, 209)";
-      container.style.backgroundColor = "";
+    radioboxes.forEach((radiobox) => {
+      radiobox.style.border = "2px solid #e3e3e3";
+      radiobox.style.backgroundColor = "";
     });
+    if (radio.checked && index === 1) {
+      radioboxes[1].style.border = "2px solid #ff6b82";
+      radioboxes[1].style.backgroundColor = "#fff9fa";
+    }
     if (radio.checked) {
-      const selectedbox = radio.closest(".radioboxContainer");
-      selectedbox.style.border = "3px solid #ff6b82";
-      selectedbox.style.backgroundColor = "#f5f0f1";
+      const selectedPrice =
+        discountPrices[index].textContent.match(/[\d.]+/)[0];
+      totals.innerHTML = `Total: $${selectedPrice} USD`;
+      console.log(
+        totals,
+        radio,
+        "radio",
+
+        index,
+        radio.checked
+      );
     }
   });
 });
